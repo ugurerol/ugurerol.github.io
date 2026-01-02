09431ae703a10fe50054c7314c6725542452ffe1
Ugur Erol <43501390+ugurerol@users.noreply.github.com>
2026-01-02 11:11:19 +0300

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 document.querySelectorAll('.sidebar a').forEach(link => {
   link.addEventListener('click', function(e){
     e.preventDefault();
-    const target = this.dataset.post;
+    const targetId = this.dataset.id;
+
     document.querySelectorAll('.post').forEach(p => p.style.display='none');
-    document.getElementById(target).style.display='block';
+    document.getElementById(targetId).style.display='block';
   });
 });
